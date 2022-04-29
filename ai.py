b94f8dc8cc2afbb818cda8880e5eb2fd30f293c4
biddls <biddls69420@gmail.com>
2022-04-29 15:50:52 +0100

    mini

--- ai.py
+++ ai.py
@@ -16,57 +16,66 @@ def nn(
         testY: pd.DataFrame
     ):
 
-    # inits the W&B serice
-    #wandb.init(project="Titanic data set")
-
-    # defines model
-    mlp_clf = MLPClassifier(hidden_layer_sizes=(10, 10),
-                            max_iter=1,
-                            activation='relu',
-                            solver='adam')
-
-    testAcc = None
-    y_pred = None
-    testAccHist = []
-    knownAcc = None
-    cEnt = None
     n_classes = np.unique(data['Survived'])
 
-    for x in range(0, 1000):
-        mlp_clf.partial_fit(data.drop('Survived', axis=1), data['Survived'], classes=n_classes)
+    for l1 in range(10, 0, -1):
+        for l2 in range(10, 0, -1):
 
-        # test model
-        y_pred = mlp_clf.predict(test)
-        y_data = mlp_clf.predict(data.drop('Survived', axis=1))
+            # defines model
+            mlp_clf = MLPClassifier(hidden_layer_sizes=(l1, l2),
+                                    max_iter=1,
+                                    activation='relu',
+                                    solver='adam')
 
-        # cross-entropy loss
-        cEnt = log_loss(testY, y_pred)
+            print("\n##### new run ####")
+            print("Layer 1 size: {}\nLayer 2 size: {}".format(l1, l2))
+            # inits the W&B serice
+            wandb.init(project="Titanic data set mini")
+            wandb.run.name = "[{}, {}]".format(l1, l2)
 
-        # return metrics
-        testAcc = accuracy_score(testY, y_pred)
-        knownAcc = accuracy_score(data['Survived'], y_data)
+            # reset data
+            testAcc = None
+            testAccHist = []
+            knownAcc = None
+            cEnt = None
 
-        testAccHist.append(testAcc)
+            for x in range(0, 1000):
+                mlp_clf.partial_fit(data.drop('Survived', axis=1), data['Survived'], classes=n_classes)
 
-        #wandb.log({"test Acc": testAcc})
-        #wandb.log({"cEnt test loss": cEnt})
-        # wandb.log({"known Acc": knownAcc})
+                # test model
+                y_pred = mlp_clf.predict(test)
+                y_data = mlp_clf.predict(data.drop('Survived', axis=1))
 
-        # early stop training if test acc is going down hill
-        if len(testAccHist) > 10:
-            if testAccHist[-10] > testAccHist[-1]:
-                break
+                # cross-entropy loss
+                cEnt = log_loss(testY, y_pred)
+
+                # return metrics
+                testAcc = accuracy_score(testY, y_pred)
+                knownAcc = accuracy_score(data['Survived'], y_data)
+
+                testAccHist.append(testAcc)
+
+                wandb.log({"test Acc": testAcc})
+                wandb.log({"cEnt test loss": cEnt})
+                wandb.log({"known Acc": knownAcc})
+
+                # early stop training if test acc is going down hill
+                # if len(testAccHist) > 10:
+                #     if testAccHist[-10] > testAccHist[-1]:
+                #         break
+
+            wandb.finish(quiet=True)
 
 
-    #print("size", len(testAccHist))
-    print('Accuracy test: {:.2f}'.format(testAcc))
-    print('cross-entropy loss: {:.2f}'.format(cEnt))
-    print('Accuracy known: {:.2f}'.format(knownAcc))
+            #print("size", len(testAccHist))
+            print('Accuracy test: {:.2f}'.format(testAcc))
+            print('cross-entropy loss: {:.2f}'.format(cEnt))
+            print('Accuracy known: {:.2f}'.format(knownAcc))
 
-    # plot graph to show performance of model
-    fig = ConfusionMatrixDisplay.from_predictions(testY, y_pred, display_labels=['dead', 'alive'])
-    fig.figure_.suptitle("Confusion Matrix for Titanic Dataset")
-    plt.show()
+    # # plot graph to show performance of model
+    # fig = ConfusionMatrixDisplay.from_predictions(testY, y_pred, display_labels=['dead', 'alive'])
+    # fig.figure_.suptitle("Confusion Matrix for Titanic Dataset")
+    # plt.show()
 
 # graphs out the av last test error for 30 runs with varying early stopping params
 def earlyStoppingTuning(
